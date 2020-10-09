f224f2f20441e089de37df83ecb2838d4e39259a
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-09 19:26:50 +0900

    Itemモデルの作成
    マイグレーションファイルの編集
    itemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_094632) do
+ActiveRecord::Schema.define(version: 2020_10_09_101926) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "shipment_score_id", null: false
+    t.integer "shipping_days_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_10_07_094632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
