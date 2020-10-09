2e964c41ca48e07cef5458f506a45a93519f57fd
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-09 09:45:51 +0900

    userモデルバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :first_name, format:{with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name Full-width characters'}
+    validates :last_name, format:{with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name Full-width characters'}
+    validates :first_name_kana,format:{with: /\A[ァ-ヶー－]+\z/, message: 'First name kana Full-width characters'}
+    validates :last_name_kana,format:{with: /\A[ァ-ヶー－]+\z/,message: 'Last name kana Full-width characters' }
+  end
+  validates :email, uniqueness: true
+  validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers'}
 end
