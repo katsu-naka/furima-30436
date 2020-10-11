a02e47bb02c5269d6398580be496775170af5d37
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-11 19:51:08 +0900

    categoryモデルの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one :order
+  belongs_to_active_hash :category
+
+  validates :title, :descripton, :category, presence: ture
+  validates :category_id, numericality: { other_than: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_101926) do
+ActiveRecord::Schema.define(version: 2020_10_11_103102) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
