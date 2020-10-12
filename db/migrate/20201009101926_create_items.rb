f7298eba5c95cbc9f252656f93f839b55fce15b3
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-12 18:15:04 +0900

    itemモデルの記述修正、バリデーション修正

--- db/migrate/20201009101926_create_items.rb
+++ db/migrate/20201009101926_create_items.rb
@@ -7,7 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :status_id, null: false
       t.integer :shipping_cost_id, null: false
       t.integer :shipment_score_id, null: false
-      t.integer :shipping_days_id, null: false
+      t.integer :shipping_day_id, null: false
       t.integer :price, null: false
       t.references :user,null: false,foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_023910) do
+ActiveRecord::Schema.define(version: 2020_10_12_034026) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_023910) do
     t.integer "status_id", null: false
     t.integer "shipping_cost_id", null: false
     t.integer "shipment_score_id", null: false
-    t.integer "shipping_days_id", null: false
+    t.integer "shipping_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
