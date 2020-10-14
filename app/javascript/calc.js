a1bee337ee1c466bfdb6605d9b2ea0cc721361c2
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-14 09:31:25 +0900

    calc.jsの修正

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,16 +1,21 @@
 function calc(){
-  const price = document.getElementById("item-price"); 
-  const tax = document.getElementById("add-tax-price"); 
-  const profit = document.getElementById("profit"); 
-  
-  price.addEventListener('input', (e) => { 
-    const priceValue = price.value;
-    //手数料の計算
-    const calcTax = Math.floor(priceValue * 0.1);  
-    tax.innerHTML = `<span id='add-tax-price'>${calcTax}</span>`;
-    //利益の計算
-    const calcProfit = Math.floor(priceValue * 0.9); 
-    profit.innerHTML = `<span id='profit'>${calcProfit}</span>`;
-  });
+  if (document.URL.match(/new/)){
+    const price = document.getElementById("item-price")
+    const tax = document.getElementById("add-tax-price")
+    const profit = document.getElementById("profit")
+    // console.log(price)
+    // console.log(tax)
+    // console.log(profit)
+    
+    price.addEventListener('input', () => { 
+      //手数料の計算
+      const calcTax = Math.floor(price.value * 0.1)
+      tax.innerHTML = `<span id='add-tax-price'>${calcTax}</span>`
+      //利益の計算
+      const calcProfit = Math.floor(price.value * 0.9)
+      profit.innerHTML = `<span id='profit'>${calcProfit}</span>`
+    })
+  }
 }
-window.addEventListener("load", calc);
+window.addEventListener('load', calc)
+
