2d813b2bfb7e4ef5f3b653edbfc0b8478a249fea
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-14 19:29:56 +0900

    addressモデルのバリデーションの編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,14 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :ken
   belongs_to :order
+
+  with_options presence: true do
+    validates :zip_number
+    validates :ken
+    validates :city_name
+    validates :block_name
+    validates :phone_number
+  end
+  validates :ken_id, numericality: { other_than: 1 , message: "Select"}
 end
