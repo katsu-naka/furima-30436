daff086ef53ba6f396bbe613e868e33133c83928
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-20 15:37:07 +0900

    検索フォーム、スタイルの編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :items do
     resources :messages, only: :create
     resources :orders, only: [:index , :create]
+    collection do
+      get 'search'
+    end
   end
 end
